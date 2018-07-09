bfdd99ade35b2b90ead52111d00f2cbe69ba4343
Saad Lahrichi <40889942+saadlahrichi@users.noreply.github.com>
2018-07-09 15:19:46 +0000

    Add files via upload

--- sum list.py
+++ sum list.py
@@ -7,7 +7,7 @@ def fun():
         if len(e)==1:
             print("You entered ",len(e), "grade.")
         else:
-            print("You entered ",len(e), "grades.")
+            print("You entered ",len(e), "grade(s).")
             choice = input("Do you want to add another grade? yes/no ")
             print("The average is ",  (sum(e)/len(e)))
 
@@ -17,10 +17,13 @@ def function():
     s=0
     n=0
     g=0
-    while g!=-1:
+    g=input("Please enter a new grade: ")
+    while g!="":
         g=eval(input("Please enter a new grade: "))
-        s=s+g
-        n=n+1
+        if g>=0:
+            s=s+g
+            n=n+1
+        else:
+            print("You entered an incorrect value")
     print("The average is ",  s/n)
-
     
